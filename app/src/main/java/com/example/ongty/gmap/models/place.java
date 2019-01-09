275ac8e960911c95ada7d760d15f27fb158717a7
tyrng <ongtypc@gmail.com>
2019-01-09 22:19:22 +0800

    prepare for address merge

--- app/src/main/java/com/example/ongty/gmap/models/place.java
+++ app/src/main/java/com/example/ongty/gmap/models/place.java
@@ -1,17 +1,21 @@
 package com.example.ongty.gmap.models;
 
+import java.util.Map;
+
 public class place {
     private String name;
     private double latitude;
     private double longitude;
+    private String address;
 
     public place() {
     }
 
-    public place(String name, double latitude, double longitude) {
+    public place(String name, double latitude, double longitude, String address) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.address = address;
     }
 
     public String getName() {
@@ -36,5 +40,13 @@ public class place {
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
